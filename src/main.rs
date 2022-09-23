8adf61dc266e275b8e6c0d4eb14e1ce68585e050
hayrudz <bedig97@gmail.com>
2022-09-23 20:07:13 +0200

    Add comments for newbieeeeeeeeees

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::io;
-use std::process;
-use dialoguer::Select;
+use std::io; // We need it to convert string into f32
+use std::process; // We need it to exit the project
+use dialoguer::Select; // We need it to have a usefull select in term
 
 fn main() -> std::io::Result<()> {
+    // I recommend to add all your code into the loop if you don't want to exit after the first run
     loop {
+        // We declare these as strings because of the io::stdin
         let mut celsius = String::new();
         let mut fahrenheit = String::new();
+        // We open the select term and give the result of answer to "selection" variable
         let selection = Select::new()
         .with_prompt("Please select the convertion you want to do")
         .item("Convert Celsius into Fahrenheit")
@@ -13,11 +16,16 @@ fn main() -> std::io::Result<()> {
         .item("Exit")
         .interact()?;
         
+        // We can use "if" but "match" works faster.
+        // You match the index of "selection"
         match selection {
             0 => {
+                // Listner, it permit to the user to type the number he want to convert
+                // We give the result to "celsius" variable
                 io::stdin()
                 .read_line(&mut celsius)
                 .expect("Failed to read the line");
+                // Result of "celsius" is converted into float by shadowing it
                 let celsius: f32 = match celsius.trim().parse() {
                     Ok(num) => num,
                     Err(_) => {
@@ -25,7 +33,9 @@ fn main() -> std::io::Result<()> {
                         continue;
                     }
                 };
+                // We also shadow "fahenrheit" to make it float
                 let fahrenheit: f32;
+                // Do I need to epxplain this line ?
                 let celsius_to_fahrenheit_formule = (celsius * 9.0/5.0) + 32.0;
                 fahrenheit = celsius_to_fahrenheit_formule;
                 println!("{celsius}°C = {fahrenheit}°F");
@@ -46,6 +56,7 @@ fn main() -> std::io::Result<()> {
                 celsius = fahrenheit_to_celsius_formule;
                 println!("{fahrenheit}°F = {celsius}°C");
             }
+            // If the user have selected "exit", exit from the app
             2 => {
                 process::exit(1);
             }
